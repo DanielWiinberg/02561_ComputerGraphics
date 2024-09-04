cce4bba08f2b148e18a05365be5cf9ae8820bf47
danieldanwii <daniel.wiinberg@norconsult.com>
2024-09-04 12:06:08 +0200

    Part 2 finished

--- 01_WebGL&GraphicsPipeline/part1/main.js
+++ 01_WebGL&GraphicsPipeline/part1/main.js
@@ -10,6 +10,6 @@ window.onload = function init(){
   var canvas = document.getElementById("webGL_Canvas");
   var gl = canvas.getContext("webgl");
   var gl = setupWebGL(canvas);
-  gl.clearColor(1.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }

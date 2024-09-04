b36e9e4f04899adcc194363dc13379caf58c8103
danieldanwii <daniel.wiinberg@norconsult.com>
2024-09-04 16:55:36 +0200

    Part 3 finished

---
+++ 01_WebGL&GraphicsPipeline/part3/main.js
@@ -0,0 +1,45 @@
+window.onload = function init(){
+  var canvas = document.getElementById("webGL_Canvas");
+
+  /** @type {WebGLRenderingContext} */
+  var gl = WebGLUtils.setupWebGL(canvas);
+
+  gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  // VERTICES
+  var vertices = [
+    vec2(0.0, 0.0), 
+    vec2(1.0, 0.0), 
+    vec2(1.0, 1.0) 
+  ];
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);   
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, "a_Position");
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);   
+  gl.enableVertexAttribArray(vPosition);
+
+  // COLORS
+  var colors = [
+    vec3(1.0, 0.0, 0.0),
+    vec3(0.0, 1.0, 0.0),
+    vec3(0.0, 0.0, 1.0)
+  ];
+
+  var cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+  var vColor = gl.getAttribLocation(program, 'a_Color');
+  gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
+
+  // DISPLAY STUFF
+  gl.drawArrays(gl.TRIANGLES, 0, colors.length);
+}
